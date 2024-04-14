f012175034e3982b43715a30dd7e680dc28717cc
Refaldy <refaldybanggana@gmail.com>
2024-04-15 05:34:27 +0700

    create register api

---
+++ src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
+import { CommonModule } from './common/common.module';
+
+@Module({
+  imports: [ConfigModule.forRoot(), UserModule, AuthModule, CommonModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
